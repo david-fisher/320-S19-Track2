2233e19ce7f202fe844cc51a1a7a2e7c4871fb70
necrotoxin44 <theunnamed97@gmail.com>
2019-04-15 15:44:21 -0400

    added stubby stubs

--- classes-unintegrated/image_handler.py
+++ classes-unintegrated/image_handler.py
@@ -1,2 +1,18 @@
 from PIL import Image
 
+
+def get_filters():
+    pass
+
+
+def get_sponsored_items():
+    pass
+
+
+def apply_filter(image, filter_id, *args):
+    pass
+
+
+def remove_filters(image):
+    pass
+
