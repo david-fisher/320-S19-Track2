34fdc7bf86d3c70eb655e7787091faac292d2198
Daniel Walsh <djwalsh@umass.edu>
2019-04-15 21:27:48 -0400

    Made changes to sponsored item insertion class in order to allow for non-square logos to be inserted and still keep their shape.

--- classes-unintegrated/sponsored_image_insert.py
+++ classes-unintegrated/sponsored_image_insert.py
@@ -8,19 +8,20 @@ class sponsoredImageInsertion:
         width, height = img.size
 
         # create a scale for insert to be resized to
-        scale = round(width/5)
+        width_scale = round(width/4)
+        height_scale = round(height/4)
 
         # create a copy of the original image
         img_copy = img.copy()
 
         # retrieve the proper sponsored item insert
-        insert = Image.open('sponsored_items/' + sponsored_item + '.png')
+        insert = Image.open('sponsored_items/' + sponsored_item + '.jpg')
 
         # resize the sponsored item
-        insert = insert.resize((scale,scale), Image.ANTIALIAS)
+        insert = insert.resize((width_scale,height_scale), Image.ANTIALIAS)
 
-        for x in range(scale):
-            for y in range(scale):
+        for x in range(width_scale):
+            for y in range(height_scale):
                 pixel = insert.getpixel((x, y))
                 img_copy.putpixel((x, y), pixel)
 
@@ -32,7 +33,11 @@ if __name__ == '__main__':
     # Open the image file and read in its data so that we can access it
     img = Image.open('../fisher.jpeg')
 
-    new_img = sponsoredImageInsertion.insert(img, "pepsi")
+   # new_img_one = sponsoredImageInsertion.insert(img, "pepsi")
+    new_img_one = sponsoredImageInsertion.insert(img, "amazon")
+    new_img_two = sponsoredImageInsertion.insert(img, "coca cola")
+
 
     # Save the image file so that we can view it
-    new_img.save('sponsored_item_pic.bmp')
+    new_img_one.save('amazon pic.jpg')
+    new_img_two.save('coca cola pic.jpg')
