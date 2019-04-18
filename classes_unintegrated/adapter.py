5740434601bddf8f18bd1e232772a4b1a0dc7293
Daniel Walsh <djwalsh@umass.edu>
2019-04-18 15:43:42 -0400

    updated adapter class

--- classes_unintegrated/adapter.py
+++ classes_unintegrated/adapter.py
@@ -4,6 +4,12 @@ test_database = {"www.google.com": "1LfIc2e5AGfyMCZ5",
                  "www.moodle.umass.edu": "SAgQoYRsvSKNVXwd",
                  "www.umass.edu": "fQCPeau1TeR643Ft"}
 
+def store_short_url(short_url):
+    pass
+
+def store_long_url(long_url):
+    pass
+
 def check_short_url(short_url):
     return None
 
@@ -21,3 +27,23 @@ def get_long_url(short_url):
 # GET request to get short_url by long_url
 def get_short_url(long_url):
     return None
+
+def store_comment(comment):
+    pass
+
+def get_original_comment(comment_id):
+    return ""
+
+def get_edited_comment(comment_id):
+    return ""
+
+def store_image(image):
+    pass
+
+def get_original_image(image_id):
+    return
+
+def get_filtered_image(image_id):
+    return
+
+def
