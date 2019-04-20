801d12ca54dfecb3b9ab959bc6f699587ee8a091
necrotoxin44 <theunnamed97@gmail.com>
2019-04-19 21:15:26 -0400

    added dynamic filter and item list creation

--- classes_unintegrated/image_handler.py
+++ classes_unintegrated/image_handler.py
@@ -1,62 +1,56 @@
-# from PIL import Image
+from PIL import Image
+import inspect
 from os import listdir
 from os.path import isfile, join
 
-import classes_unintegrated.adapter as Adapter
-import classes_unintegrated.filters as Filters
+import adapter
+import filters
 
 items_dir_path = 'sponsored_items'
-item_files = [f for f in listdir(items_dir_path) if isfile(join(items_dir_path, f))]
+
 
 class ImageFilterHandler:
 
-    filters = "grayscale", "club filter", "sepia", "negative", "flip", "mirror"
-    sponsored_items = "coca cola", "coca cola can", "amazon", "pepsi"
+    # retrieves filter class names from filters module
+    filters = [f[0] for f in inspect.getmembers(filters, inspect.isclass) if f[1].__module__ == filters.__name__]
+    # retrieves sponsored_items files names from sponsored_items directory
+    sponsored_items = [f for f in listdir(items_dir_path) if isfile(join(items_dir_path, f))]
     original_image = ""
     modified_image = ""
     done_by_admin = False
-
 
     def get_filters(self):
         # Currently only returns a list of filter names
         return self.filters
 
-
     def get_sponsored_items(self):
         # Currently only returns a list of sponsored item names
         return self.sponsored_items
 
-    def apply_filter(self, image, filter_id, *args):
-        # Currently just comparing against a string.
+    @staticmethod
+    def apply_filter(image, filter_id, *args):
         # Eventually should be implemented with filter list from DB
-        if filter_id == "grayscale":
-            modified_image = Filters.Grayscale.filter(image)
+        if hasattr(filters, filter_id):
+            filter_class = getattr(filters, filter_id)
+            return filter_class.filter(image)
+        else:
+            # invalid filter_id
+            print('invalid filter_id')
+            return "uh oh, should probably do something about this"
 
-        if filter_id == "club filter":
-            modified_image = Filters.ClubFilter.filter(image)
-
-        if filter_id == "sepia":
-            modified_image = Filters.Sepia.filter(image)
-
-        if filter_id == "negative":
-            modified_image = Filters.Negative.filter(image)
-
-        if filter_id == "flip":
-            modified_image = Filters.Flip.filter(image)
-
-        if filter_id == "mirror":
-            modified_image = Filters.Mirror.filter(image)
-
-        return modified_image
-
-
-    def remove_filters(image):
+    def remove_filters(self, image):
         pass
 
-    def __save_image(image):
-        Adapter.store_image(image)
+    def __save_image(self, image):
+        adapter.store_image(image)
 
 
 if __name__ == '__main__':
-    print(item_files)
+    print([f[0] for f in inspect.getmembers(filters, inspect.isclass) if f[1].__module__ == filters.__name__])
+    print(ImageFilterHandler.filters)
+    print(ImageFilterHandler.sponsored_items)
+
+    # img = Image.open('../fisher.jpeg')
+    # new_img_one = ImageFilterHandler.apply_filter(img, 'ClubFilter')
+    # new_img_one.save('club_filter.jpg')
 
