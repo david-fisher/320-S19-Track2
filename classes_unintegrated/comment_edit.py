a09c9cbe25f37e42de9ee4c64943b6f0265ab37e
Daniel Walsh <djwalsh@umass.edu>
2019-04-20 20:05:50 -0400

    Created a function to parse hastags out of a string.

--- classes_unintegrated/comment_edit.py
+++ classes_unintegrated/comment_edit.py
@@ -7,5 +7,9 @@ class EditComment:
         new_comment = original_comment + edit
         DBAdapter.store_comment(new_comment)
 
-
-
+    def parse_hashtags(hashtags):
+        list_of_hashtags = []
+        for word in hashtags.split():
+            if word[0] == "#":
+                list_of_hashtags.append(word)
+        return list_of_hashtags
