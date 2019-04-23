8372a8455d89c94d929e1407c03a5d188aaf31d1
LITTLE-BERTHA <robertjewell808@gmail.com>
2019-04-23 17:47:45 -0400

    Fix adapter; Use shake_128 for create_short_url

--- classes_unintegrated/url_shortener.py
+++ classes_unintegrated/url_shortener.py
@@ -1,38 +1,43 @@
-
+import hashlib
 import random
-import string
 
-import classes_unintegrated.adapter as DBAdapter
+import classes_unintegrated.adapter as db_adapter
 
 
 # user inputs url, see if it's in the database
-def long_url_exists(lUrl):
-    return DBAdapter.check_long_url(lUrl)
+def long_url_exists(long_link):
+    return db_adapter.check_long_url(long_link)
 
 
 # generate random ascii string
-def create_short_url(longLink):
-    shortLink = ''.join(
-        random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
-    # check if it is within the text file
-    while DBAdapter.check_short_url(shortLink):
-        shortLink = ''.join(
-            random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
-    return shortLink
+def create_short_url():
+    m = hashlib.shake_128()
+    m.update(str(random.getrandbits(256)).encode('utf-8'))
+    short_link = m.hexdigest(4)
+
+    # check if it is within the db
+    while db_adapter.check_short_url(short_link):
+        m.update(str(random.getrandbits(256)).encode('utf-8'))
+        short_link = m.hexdigest(4)
+
+    return short_link
 
 
 # retrieve short_url
-def get_short_url(longLink):
-    check = long_url_exists(longLink)
+def get_short_url(long_link):
+    check = long_url_exists(long_link)
     if check:
-        return DBAdapter.get_short_url(longLink)
+        return db_adapter.get_short_url(long_link)
     else:
-        return create_short_url(longLink)
+        return create_short_url()
 
 
 # redirect a user clicking a long url to a shortened one
-def get_long_url(sUrl):
-    return DBAdapter.get_long_url(sUrl)
+def get_long_url(short_link):
+    if db_adapter.check_short_url(short_link):
+        return db_adapter.get_long_url(short_link)
+    else:
+        return None
 
 print(get_short_url("www.umass.edu"))
 print(get_long_url('www.membersonly.com/fQCPeau1TeR643Ft'))
