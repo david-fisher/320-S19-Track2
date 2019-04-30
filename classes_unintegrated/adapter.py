d4869bf85f64a2b2efb5b6a9b3a210591523cd6c
Daniel Walsh <djwalsh@umass.edu>
2019-04-30 16:56:52 -0400

    Implemented some of the DBAdapter class.

--- classes_unintegrated/adapter.py
+++ classes_unintegrated/adapter.py
@@ -1,44 +1,68 @@
 # GET request to check if short_url exists
+
+from members_only.models import ShortLink, Comment, Image
+
 test_database = {"www.google.com": "1LfIc2e5AGfyMCZ5",
                  "www.moodle.umass.edu": "SAgQoYRsvSKNVXwd",
                  "www.umass.edu": "fQCPeau1TeR643Ft"}
 
-def store_short_url(short_url):
-    pass
+class DBAdapter:
 
-def store_long_url(long_url):
-    pass
+    @staticmethod
+    def store_short_url(short_url):
+        pass
 
-def check_short_url(short_url):
-    return False
+    @staticmethod
+    def store_long_url(long_url):
+        pass
 
-# GET request to check if long_url exists
-def check_long_url(long_url):
-    return False
+    @staticmethod
+    def check_short_url(short_url):
+        return False
 
-# GET request to get long_url by short_url
-def get_long_url(short_url):
-    return None
+    @staticmethod
+    # GET request to check if long_url exists
+    def check_long_url(long_url):
+        return False
 
-# GET request to get short_url by long_url
-def get_short_url(long_url):
-    return None
+    @staticmethod
+    # GET request to get long_url by short_url
+    def get_long_url(short_url):
+        long_url = ShortLink.objects.get(short_url=short_url)
+        return long_url
 
-def store_comment(comment):
-    return None
+    @staticmethod
+    # GET request to get short_url by long_url
+    def get_short_url(long_url):
+        short_url = ShortLink.objects.get(originalURL=long_url)
+        return short_url
 
-def get_original_comment(comment_id):
-    return ""
+    @staticmethod
+    def store_comment(comment):
+        comment_to_store = Comment.objects.create(content=comment)
+        comment_to_store.save()
 
-def get_edited_comment(comment_id):
-    return ""
+    @staticmethod
+    def get_original_comment(comment_id):
+        original_comment = Comment.objects.get(comment_id=comment_id)
+        return original_comment
 
-def store_image(image):
-    pass
+    @staticmethod
+    def get_edited_comment(comment_id):
+        original_comment = Comment.objects.get(comment_id=comment_id)
+        return original_comment
 
-def get_original_image(image_id):
-    return
+    @staticmethod
+    def store_image(image):
+        image_to_store = Image.objects.create(current_image=image)
+        image_to_store.save()
 
-def get_filtered_image(image_id):
-    return
+    @staticmethod
+    def get_original_image(image_id):
+        original_image = Image.objects.get(image_id=image_id)
+        return original_image
 
+    @staticmethod
+    def get_filtered_image(image_id):
+        filtered_image = Image.objects.get(image_id=image_id)
+        return filtered_image
