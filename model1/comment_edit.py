a1ecb871205431db1723d5f65859041a601da8a8
Daniel Walsh <djwalsh@umass.edu>
2019-04-30 16:28:15 -0400

    Modified Comment Edit Class.

--- model1/comment_edit.py
+++ model1/comment_edit.py
@@ -3,10 +3,24 @@ from members_only.models import Comment
 class EditComment:
 
     def edit_comment(comment_id, edit):
+        # Retrieve the original comment to be modified
         parent_comment = Comment.objects.get(id=comment_id)
+
+        # Sets original_comment as a string of the content in the parent_comment
         original_comment = parent_comment.content
+
+        # Creates a new string by appending the edit to the end of the original comment string
         new_comment = original_comment + edit
-        Comment.objects.create(parent_id = parent_comment.id, content=new_comment)
+
+        # Creates a new Comment object with a parent_id value as well as the content to be stored
+        edited_comment = Comment.objects.create(parent_id = parent_comment.id, content=new_comment)
+
+        # Save the Comment Object to the DB
+        edited_comment.save()
+
+
+    """Currently unnecessary but if Front End decides that they want 
+    the edits displayed as a list then we would use this method."""
 
     def parse_hashtags(hashtags):
         list_of_hashtags = []
