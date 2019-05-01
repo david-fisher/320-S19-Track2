e1aa7e3bbd66bc5145155c9a9bc2c66e37f175d0
LITTLE-BERTHA <robertjewell808@gmail.com>
2019-05-01 18:54:51 -0400

    Fix import and url storing

--- classes_unintegrated/url_shortener.py
+++ classes_unintegrated/url_shortener.py
@@ -1,6 +1,6 @@
 import random
 
-import classes_unintegrated.adapter as db_adapter
+from classes_unintegrated.adapter import DBAdapter as dbAdapter
 
 
 class URLShortener:
@@ -8,7 +8,7 @@ class URLShortener:
     # user inputs url, see if it's in the database
     @staticmethod
     def long_url_exists(long_link):
-        return db_adapter.check_long_url(long_link)
+        return dbAdapter.check_long_url(long_link)
 
     # generate random ascii string
     @staticmethod
@@ -16,10 +16,10 @@ class URLShortener:
         token = str(hex(random.getrandbits(32)))[2:]
 
         # check if it is within the db
-        while db_adapter.check_short_url(token):
+        while dbAdapter.check_short_url(token):
             token = str(hex(random.getrandbits(32)))[2:]
-        db_adapter.store_short_url(token)
-        db_adapter.store_long_url(long_link)
+
+        dbAdapter.store_url(token, long_link)
         return token
 
     # retrieve short_url
@@ -27,7 +27,7 @@ class URLShortener:
     def get_short_url(long_link):
         check = URLShortener.long_url_exists(long_link)
         if check:
-            return db_adapter.get_short_url(long_link)
+            return dbAdapter.get_short_url(long_link)
         else:
             return URLShortener.create_short_url(long_link)
 
@@ -35,11 +35,7 @@ class URLShortener:
     # redirect a user clicking a long url to a shortened one
     @staticmethod
     def get_long_url(short_link):
-        if db_adapter.check_short_url(short_link):
-            return db_adapter.get_long_url(short_link)
+        if dbAdapter.check_short_url(short_link):
+            return dbAdapter.get_long_url(short_link)
         else:
             return None
-
-print(URLShortener.get_short_url("www.umass.edu"))
-print(URLShortener.get_long_url('www.membersonly.com/fQCPeau1TeR643Ft'))
-#print(validateShortenedURL('1234567812345679'))
